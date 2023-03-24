7e4ef1760216efb0610a89e4caaaed6848186610
sosokbomza <sava.ognev@gmail.com>
2023-03-24 15:29:01 +0100

    Initial commit

---
+++ Source/Thief/ThiefGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThiefGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThiefGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AThiefGameMode();
+};
+
+
+
